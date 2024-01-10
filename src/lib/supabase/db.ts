70b765a36667b33966630c88ea6b9e3a63070962
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2024-01-10 13:13:23 +0530

    Refactor db initialization in supabase/db.ts

--- src/lib/supabase/db.ts
+++ src/lib/supabase/db.ts
@@ -11,7 +11,7 @@ if (!process.env.DATABASE_URL) {
 }
 
 const client = postgres(process.env.DATABASE_URL as string, { max: 1 });
-const db = drizzle(client, schema);
+const db = drizzle(client, { schema });
 
 const migrateDb = async () => {
   try {
