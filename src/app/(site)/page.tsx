eb9786d81bb566be92c82afbb2543f05759b9828
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2024-01-10 15:18:59 +0530

    Add calendar image to homepage

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Banner from "../../../public/appBanner.png";
 import { CLIENTS } from "@/lib/constants";
+import Cal from "../../../public/cal.png";
 
 const HomePage = () => {
   return (
@@ -126,6 +127,54 @@ const HomePage = () => {
               ))}
             </div>
           ))}
+        </div>
+      </section>
+
+      <section
+        className="px-4
+        sm:px-6
+        flex
+        justify-center
+        items-center
+        flex-col
+        relative
+      "
+      >
+        <div
+          className="w-[30%]
+          blur-[120px]
+          rounded-full
+          h-32
+          absolute
+          bg-brand-primaryPurple/50
+          -z-10
+          top-22
+        "
+        />
+        <TitleSection
+          title="Keep track of your meetings all in one place"
+          subheading="Capture your ideas, thoughts, and meeting notes in a structured and organized manner."
+          pill="Features"
+        />
+        <div
+          className="mt-10
+          max-w-[450px]
+          flex
+          justify-center
+          items-center
+          relative
+          sm:ml-0
+          rounded-2xl
+          border-8
+          border-washed-purple-300 
+          border-opacity-10
+        "
+        >
+          <Image
+            src={Cal}
+            alt="Banner"
+            className="rounded-2xl"
+          />
         </div>
       </section>
     </>
