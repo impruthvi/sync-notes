bea40ad0cd8230412a4976e3be07164ba020ef7a
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2024-01-10 15:14:52 +0530

    Add client logos and animation to homepage

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -2,11 +2,12 @@ import TitleSection from "@/components/landing-page/title-section";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Banner from "../../../public/appBanner.png";
+import { CLIENTS } from "@/lib/constants";
 
 const HomePage = () => {
   return (
-    <section>
-      <div className="overflow-hidden px-4 sm:px-6 mt-10 sm:flex sm:flex-col gap-4 md:justify-center md:items-center">
+    <>
+      <section className="overflow-hidden px-4 sm:px-6 mt-10 sm:flex sm:flex-col gap-4 md:justify-center md:items-center">
         <TitleSection
           pill="✨ Your workspace, Perfected"
           title="All-In-One Collaboration and Productivity Platform"
@@ -63,8 +64,71 @@ const HomePage = () => {
           "
           ></div>
         </div>
-      </div>
-    </section>
+      </section>
+
+      <section className="relative">
+        <div
+          className="overflow-hidden
+             flex
+             after:content['']
+             after:dark:from-brand-dark
+             after:to-transparent
+             after:from-background
+             after:bg-gradient-to-l
+             after:right-0
+             after:bottom-0
+             after:top-0
+             after:w-20
+             after:z-10
+             after:absolute
+   
+             before:content['']
+             before:dark:from-brand-dark
+             before:to-transparent
+             before:from-background
+             before:bg-gradient-to-r
+             before:left-0
+             before:top-0
+             before:bottom-0
+             before:w-20
+             before:z-10
+             before:absolute
+
+
+        "
+        >
+          {[...Array(2)].map((arr) => (
+            <div
+              key={arr}
+              className="flex
+                flex-nowrap
+                animate-slide
+          "
+            >
+              {CLIENTS.map((client) => (
+                <div
+                  key={client.alt}
+                  className=" relative
+                    w-[200px]
+                    m-20
+                    shrink-0
+                    flex
+                    items-center
+                  "
+                >
+                  <Image
+                    src={client.logo}
+                    alt={client.alt}
+                    width={200}
+                    className="object-contain max-w-none"
+                  />
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </section>
+    </>
   );
 };
 
---
+++ src/lib/constants.ts
@@ -0,0 +1,13 @@
+import client1 from "../../public/client1.png";
+import client2 from "../../public/client2.png";
+import client3 from "../../public/client3.png";
+import client4 from "../../public/client4.png";
+import client5 from "../../public/client5.png";
+
+export const CLIENTS = [
+  { alt: "client1", logo: client1 },
+  { alt: "client2", logo: client2 },
+  { alt: "client3", logo: client3 },
+  { alt: "client4", logo: client4 },
+  { alt: "client5", logo: client5 },
+];
