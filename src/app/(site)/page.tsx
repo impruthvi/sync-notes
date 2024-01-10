711ebe83ce3cbe28b780de91e5aca57c194008ad
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2024-01-10 15:33:03 +0530

    Add new icons and update button component

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -2,13 +2,15 @@ import TitleSection from "@/components/landing-page/title-section";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Banner from "../../../public/appBanner.png";
-import { CLIENTS, USERS } from "@/lib/constants";
+import { CLIENTS, PRICING_CARDS, PRICING_PLANS, USERS } from "@/lib/constants";
 import Cal from "../../../public/cal.png";
+import Diamond from '../../../public/icons/diamond.svg';
+import CheckIcon from '../../../public/icons/check.svg';
 import CustomCard from "@/components/landing-page/custom-card";
 import { randomUUID } from "crypto";
 import { twMerge } from "tailwind-merge";
 import clsx from "clsx";
-import { CardContent, CardDescription, CardTitle } from '@/components/ui/card';
+import { CardContent, CardDescription, CardTitle } from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const HomePage = () => {
@@ -34,7 +36,7 @@ const HomePage = () => {
         "
         >
           <Button
-            variant="secondary"
+            variant="btn-secondary"
             className="w-full 
                 rounded-[10px]
                 p-6
@@ -259,6 +261,122 @@ const HomePage = () => {
                 ></CustomCard>
               ))}
             </div>
+          ))}
+        </div>
+      </section>
+
+      <section
+        className="mt-20
+        px-4
+        sm:px-6
+      "
+      >
+        <TitleSection
+          title="The Perfect Plan For You"
+          subheading="Experience all the benefits of our platform. Select a plan that suits your needs and take your productivity to new heights."
+          pill="Pricing"
+        />
+        <div
+          className="flex 
+        flex-col-reverse
+        sm:flex-row
+        gap-4
+        justify-center
+        sm:items-stretch
+        items-center
+        mt-10
+        "
+        >
+          {PRICING_CARDS.map((card) => (
+            <CustomCard
+              key={card.planType}
+              className={clsx(
+                "w-[300px] rounded-2xl dark:bg-black/40 background-blur-3xl relative",
+                {
+                  "border-brand-primaryPurple/70":
+                    card.planType === PRICING_PLANS.proplan,
+                }
+              )}
+              cardHeader={
+                <CardTitle
+                  className="text-2xl
+                  font-semibold
+              "
+                >
+                  {card.planType === PRICING_PLANS.proplan && (
+                    <>
+                      <div
+                        className="hidden dark:block w-full blur-[120px] rounded-full h-32
+                        absolute
+                        bg-brand-primaryPurple/80
+                        -z-10
+                        top-0
+                      "
+                      />
+                      <Image
+                        src={Diamond}
+                        alt="Pro Plan Icon"
+                        className="absolute top-6 right-6"
+                      />
+                    </>
+                  )}
+                  {card.planType}
+                </CardTitle>
+              }
+              cardContent={
+                <CardContent className="p-0">
+                  <span
+                    className="font-normal 
+                    text-2xl
+                "
+                  >
+                    ${card.price}
+                  </span>
+                  {+card.price > 0 ? (
+                    <span className="dark:text-washed-purple-800 ml-1">
+                      /mo
+                    </span>
+                  ) : (
+                    ""
+                  )}
+                  <p className="dark:text-washed-purple-800">
+                    {card.description}
+                  </p>
+                  <Button
+                    variant="btn-primary"
+                    className="whitespace-nowrap w-full mt-4"
+                  >
+                    {card.planType === PRICING_PLANS.proplan
+                      ? "Go Pro"
+                      : "Get Started"}
+                  </Button>
+                </CardContent>
+              }
+              cardFooter={
+                <ul
+                  className="font-normal
+                  flex
+                  mb-2
+                  flex-col
+                  gap-4
+                "
+                >
+                  <small>{card.highlightFeature}</small>
+                  {card.freatures.map((feature) => (
+                    <li
+                      key={feature}
+                      className="flex
+                      items-center
+                      gap-2
+                    "
+                    >
+                      <Image src={CheckIcon} alt="Check Icon" />
+                      {feature}
+                    </li>
+                  ))}
+                </ul>
+              }
+            />
           ))}
         </div>
       </section>
