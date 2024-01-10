675f8d42bde24ab3ce727c9c241e895219e3de5f
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2024-01-10 17:29:09 +0530

    Add authentication callback route and related functions

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -4,3 +4,20 @@ export const FormSchema = z.object({
   email: z.string().describe("Email").email({ message: "Invalid Email" }),
   password: z.string().describe("Password").min(1, "Password is required"),
 });
+
+export const SignUpFormSchema = z
+  .object({
+    email: z.string().describe("Email").email({ message: "Invalid Email" }),
+    password: z
+      .string()
+      .describe("Password")
+      .min(6, "Password must be minimum 6 characters"),
+    confirmPassword: z
+      .string()
+      .describe("Confirm Password")
+      .min(6, "Password must be minimum 6 characters"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match.",
+    path: ["confirmPassword"],
+  });
