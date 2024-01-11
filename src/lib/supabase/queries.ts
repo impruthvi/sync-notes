051491182da7a89d7973a75ba7446a653a8df0a5
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2024-01-11 11:42:56 +0530

    Implementing create workspace

---
+++ src/lib/supabase/queries.ts
@@ -0,0 +1,27 @@
+"use server";
+import db from "./db";
+import { workspaces } from "./schema";
+import { Subscription, workspace } from "./supabase.types";
+
+export const createWorkspace = async (workspace: workspace) => {
+  try {
+    const response = await db.insert(workspaces).values(workspace);
+    return { data: null, error: null };
+  } catch (error) {
+    console.log(error);
+    return { data: null, error: "Error" };
+  }
+};
+
+export const getUserSubscriptionStatus = async (userId: string) => {
+  try {
+    const data = await db.query.subscriptions.findFirst({
+      where: (s, { eq }) => eq(s.userId, userId),
+    });
+    if (data) return { data: data as Subscription, error: null };
+    else return { data: null, error: null };
+  } catch (error) {
+    console.log(error);
+    return { data: null, error: `Error` };
+  }
+};
