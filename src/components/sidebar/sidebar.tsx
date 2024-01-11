b85cdf8871617c268e76ee2ce0c27f18b0b83fa1
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2024-01-11 18:06:03 +0530

    Add layout and page components for workspace dashboard

---
+++ src/components/sidebar/sidebar.tsx
@@ -0,0 +1,60 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import React from "react";
+
+import { cookies } from "next/headers";
+
+import { redirect } from "next/navigation";
+import {
+  getCollaboratingWorkspaces,
+  getFolders,
+  getPrivateWorkspaces,
+  getSharedWorkspaces,
+  getUserSubscriptionStatus,
+} from "@/lib/supabase/queries";
+import { twMerge } from "tailwind-merge";
+interface SidebarProps {
+  params: { workspaceId: string };
+  className?: string;
+}
+
+const Sidebar: React.FC<SidebarProps> = async ({ params, className }) => {
+  const supabase = createServerComponentClient({ cookies });
+  //user
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) return;
+
+  //subscr
+  const { data: subscriptionData, error: subscriptionError } =
+    await getUserSubscriptionStatus(user.id);
+
+  //folders
+  const { data: workspaceFolderData, error: foldersError } = await getFolders(
+    params.workspaceId
+  );
+  //error
+  if (subscriptionError || foldersError) redirect("/dashboard");
+
+  const [privateWorkspaces, collaboratingWorkspaces, sharedWorkspaces] =
+    await Promise.all([
+      getPrivateWorkspaces(user.id),
+      getCollaboratingWorkspaces(user.id),
+      getSharedWorkspaces(user.id),
+    ]);
+
+  //get all the different workspaces private collaborating shared
+  return (
+    <aside
+      className={twMerge(
+        "hidden sm:flex sm:flex-col w-[280px] shrink-0 p-4 md:gap-4 !justify-between",
+        className
+      )}
+    >
+      Hello
+    </aside>
+  );
+};
+
+export default Sidebar;
