3d91ccb0d432dee51ae4a367d95502b53e137f3b
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2024-01-16 14:58:43 +0530

    Update Supabase domain in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["fmliqecnrxgzwvucipeu.supabase.co"],
+    domains: ["rgtwymltgyoyhhkilvpt.supabase.co"],
   },
 };
 
