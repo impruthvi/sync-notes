bc850a6a21fad0a2a16995462af1a5ec72f6c690
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2024-01-18 15:30:40 +0530

    Add utility functions for date conversion and API communication

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -14,3 +14,38 @@ export const formatPrice = (price: Price) => {
   }).format((price?.unitAmount || 0) / 100);
   return priceString;
 };
+
+export const toDateTime = (secs: number) => {
+  var t = new Date("1970-01-01T00:30:00Z");
+  t.setSeconds(secs);
+  return t;
+};
+
+export const postData = async ({
+  url,
+  data,
+}: {
+  url: string;
+  data?: { price: Price };
+}) => {
+  console.log("posting,", url, data);
+  const res: Response = await fetch(url, {
+    method: "POST",
+    headers: new Headers({ "Content-Type": "application/json" }),
+    credentials: "same-origin",
+    body: JSON.stringify(data),
+  });
+  if (!res.ok) {
+    console.log("Error in postData", { url, data, res });
+    throw Error(res.statusText);
+  }
+  return res.json();
+};
+
+export const getURL = () => {
+  let url = process?.env?.NEXT_PUBLIC_SITE_URL ?? "http://localhost:3000/";
+
+  url = url.includes("http") ? url : `https://${url}`;
+  url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
+  return url;
+};
