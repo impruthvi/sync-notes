09e686592aa2792cc8519e3d4bf33e3ca02a721a
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2024-01-18 14:21:00 +0530

    Add real-time workspace updates to useSupabaseRealtime hook

--- src/lib/hooks/useSupabaseRealtime.tsx
+++ src/lib/hooks/useSupabaseRealtime.tsx
@@ -2,11 +2,13 @@ import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import React, { useEffect } from "react";
 import { useAppState } from "../providers/state-provider";
 
-import { File, Folder } from "../supabase/supabase.types";
+import { File, Folder, workspace } from "../supabase/supabase.types";
 import { useRouter } from "next/navigation";
+import { useSupabaseUser } from "../providers/supabase-user-provider";
 
 const useSupabaseRealtime = () => {
   const supabase = createClientComponentClient();
+  const { user } = useSupabaseUser();
   const { dispatch, state, workspaceId: selectedWorskpace } = useAppState();
   const router = useRouter();
   useEffect(() => {
@@ -164,6 +166,65 @@ const useSupabaseRealtime = () => {
                 }
               })
             );
+          }
+        }
+      )
+      .on(
+        "postgres_changes",
+        { event: "*", schema: "public", table: "workspaces" },
+        async (payload) => {
+          if (payload.eventType === "INSERT") {
+            const { id: workspaceId } = payload.new;
+            if (
+              !state.workspaces.find(
+                (workspace) => workspace.id === workspaceId
+              )
+            ) {
+              if(!user) return;
+              const newWorkspace: workspace = {
+                id: payload.new.id,
+                createdAt: payload.new.created_at,
+                title: payload.new.title,
+                inTrash: payload.new.in_trash,
+                iconId: payload.new.icon_id,
+                bannerUrl: payload.new.banner_url,
+                data: payload.new.data,
+                workspaceOwner: user.id,
+                logo: payload.new.logo,
+              };
+              dispatch({
+                type: "ADD_WORKSPACE",
+                payload: { ...newWorkspace, folders: [] },
+              });
+            }
+          } else if (payload.eventType === "DELETE") {
+            const workspaceExists = state.workspaces.some((workspace) => {
+              if (workspace.id === payload.old.id) {
+                return true;
+              }
+            });
+            if (workspaceExists) {
+              router.replace("/dashboard");
+              dispatch({ type: "DELETE_WORKSPACE", payload: payload.old.id });
+            }
+          } else if (payload.eventType === "UPDATE") {
+            state.workspaces.some((workspace) => {
+              if (workspace.id === payload.new.id) {
+                dispatch({
+                  type: "UPDATE_WORKSPACE",
+                  payload: {
+                    workspaceId: payload.new.id,
+                    workspace: {
+                      title: payload.new.title,
+                      iconId: payload.new.icon_id,
+                      bannerUrl: payload.new.banner_url,
+                      inTrash: payload.new.in_trash,
+                    },
+                  },
+                });
+                return true;
+              }
+            });
           }
         }
       )
